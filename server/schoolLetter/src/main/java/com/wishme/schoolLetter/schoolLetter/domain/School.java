799d2f2ed1f10f51d83bc817510947d607e48193
이효경 <gyrud0482@naver.com>
2023-10-24 09:21:09 +0900

    Fix : school entity 수정

--- server/schoolLetter/src/main/java/com/wishme/schoolLetter/schoolLetter/domain/School.java
+++ server/schoolLetter/src/main/java/com/wishme/schoolLetter/schoolLetter/domain/School.java
@@ -1,6 +1,7 @@
 package com.wishme.schoolLetter.schoolLetter.domain;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,13 +15,19 @@ public class School {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "school_name", unique = true, nullable = false)
+    @Column(unique = true, nullable = false)
     private Integer schoolSeq;
 
-    @Column(nullable = false)
+    @Column( nullable = false)
     private String schoolName;
 
     @Column(nullable = false)
     private String region;
 
+    @Builder
+    public School(Integer schoolSeq, String schoolName, String region) {
+        this.schoolSeq = schoolSeq;
+        this.schoolName = schoolName;
+        this.region = region;
+    }
 }
