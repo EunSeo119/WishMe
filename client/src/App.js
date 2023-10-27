5457a5c3579085330b62ce717b76e5c431671b1d
최한윤 <chlgksdbs98@naver.com>
2023-10-27 11:05:31 +0900

    Fix: 충돌 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-<<<<<<< HEAD
 import MainPage from './pages/mainPage/mainPage'
-import DeskPage from './pages/deskPage'
-=======
-import MainPage from './pages/mainPage'
 import DeskPage from './pages/deskPage/deskPage'
->>>>>>> e16a8e67bf53d55c46a6536c0f6b7ad7261e0607
 import SchoolPage from './pages/schoolPage/schoolPage'
 import WriteDeskLetter from './pages/deskLetter/writeDeskLetter'
 import style from './app.module.css'
