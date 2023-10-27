a5f96a7ee9dbbef3a1fef8241b6128f7b4ddd6bf
chlgksdbs <chlgksdbs98@naver.com>
2023-10-27 16:09:27 +0900

    Feat: KakaoLogin 연동

--- client/src/pages/mainPage/mainPage.js
+++ client/src/pages/mainPage/mainPage.js
@@ -3,6 +3,14 @@ import '../../fonts/font.css';
 
 const MainPage = () => {
 
+    const REST_API_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
+    const REDIRECT_URI = 'http://localhost:8080/kakao/callback';
+    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+    const kakaoLogin = () => {
+        window.location.href = kakaoURL;
+    };
+
     return (
         <div className={styleMain.bg}>
             <div className={styleMain.topbox}>
@@ -15,7 +23,7 @@ const MainPage = () => {
                 </div>
             </div>
             <div className={styleMain.bottombox}>
-                <div className={styleMain.kakao}>
+                <div className={styleMain.kakao} onClick={kakaoLogin}>
                     <img src="https://wishme-bichnali.s3.ap-northeast-2.amazonaws.com/login/kakao_login_medium_wide.png"></img>
                 </div>
                 <div className={styleMain.nologin}>
