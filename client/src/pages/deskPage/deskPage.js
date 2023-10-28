98c6f584960f28a2c95a6810ecaff840b956f8bb
eunseo <qnrqjrm03@naver.com>
2023-10-29 00:19:51 +0900

    [FE/Feat] 편지 작성 후 등록

--- client/src/pages/deskPage/deskPage.js
+++ client/src/pages/deskPage/deskPage.js
@@ -45,13 +45,13 @@ const DeskPage = () => {
             <div className={style.deskPage}>
                 <img src="https://wishme-bichnali.s3.ap-northeast-2.amazonaws.com/background/deskBackground.PNG" className={styleApp.bg} />
                 <div className={style.board}>
-                    <div className={style.title}><b>{ }수능대박1</b>님의 책상에<br /><b>{ }246개</b>의 응원이 왔어요!</div>
+                    <div className={style.title}><b>{ }수능대박2</b>님의 책상에<br /><b>{ }246개</b>의 응원이 왔어요!</div>
                 </div>
                 <div className={style.desk}>
 
                 </div>
                 <div className={style.btn}>
-                    <Link to="/desk/write" className={style.link}>
+                    <Link to="/desk/selectAsset" className={style.link}>
                         <div className={style.cheerUpBtn}>응원하기</div>
                     </Link>
                     <div className={style.myDeskBtn}>내 책상 보기</div>
