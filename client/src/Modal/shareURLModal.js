c68c00fbe71355a297fbbe490f391ff22c1b247b
zosunny <zoheyy108@naver.com>
2023-10-30 00:54:39 +0900

    [FE/Feat] : 카카오톡 공유하기 API 설정

--- client/src/Modal/shareURLModal.js
+++ client/src/Modal/shareURLModal.js
@@ -1,7 +1,44 @@
 import React from "react";
 import style from "./shareURLModal.module.css";
+import { useEffect } from "react";
+
+// 카카오 공유하기 기능
+const { Kakao } = window;
 
 function ShareURLModal({ isOpen, onClose }) {
+  const realUrl = "http://k9b205.p.ssafy.io";
+  const localUrl = window.location.href;
+  const SHARE_URL_KEY = process.env.REACT_APP_KAKAO_REST_API_KEY;
+
+  useEffect(() => {
+    Kakao.cleanup();
+    Kakao.init(SHARE_URL_KEY);
+    // 잘 적용됐으면 true 리턴
+    console.log(Kakao.isInitialized());
+  }, []);
+
+  const shareKakao = () => {
+    Kakao.Share.sendDefault({
+      objectType: "feed",
+      content: {
+        title: "Wish Me, 나의 행운을 빌어줘",
+        description: "수험생을 위한 응원 편지를 보내주세요 :)",
+        imageUrl: "https://wishme-bichnali.s3.ap-northeast-2.amazonaws.com/login/clover.png",
+        link: {
+          mobileWebUrl: localUrl,
+        },
+      },
+      buttons: [
+        {
+          title: "응원 편지 작성하기",
+          link: {
+            mobileWebUrl: localUrl,
+          },
+        },
+      ],
+    });
+  };
+
   if (!isOpen) return null;
 
   return (
@@ -13,7 +50,14 @@ function ShareURLModal({ isOpen, onClose }) {
       <div className={style.title}>공유하기</div>
       {/* </div> */}
       <div className={style.btn}>
-        <div className={style.kakaoBtn}>카카오톡 공유하기</div>
+        <div
+          className={style.kakaoBtn}
+          onClick={() => {
+            shareKakao();
+          }}
+        >
+          카카오톡 공유하기
+        </div>
         <div className={style.LinkBtn}>링크 복사하기</div>
       </div>
     </div>
--- client/src/pages/deskPage/deskPage.js
+++ client/src/pages/deskPage/deskPage.js
@@ -54,67 +54,51 @@ const DeskPage = () => {
       });
   }, [userUuid, page]);
 
-  useEffect(() => {
-    axios
-      .get(`/api/my/letter/all/${userUuid}?page=${page}`)
-      .then((response) => {
-        const data = response.data;
-        console.log(data);
-        setDeskName(data.toUserNickname);
-        setTotalCount(data.totalLetterCount);
-        setDeskLetter(data.myLetterResponseDtoList);
-        // setTotalPage(data.totalPage)
-      })
-      .catch((error) => {
-        console.error("API 요청 중 오류 발생:", error);
-      });
-  }, [page]);
-
   return (
-    <div className={styleApp.app}>
-      <div className={style.deskPage}>
-        <img
-          src="https://wishme-bichnali.s3.ap-northeast-2.amazonaws.com/background/deskBackground.PNG"
-          className={styleApp.bg}
-        />
-        <div className={style.board}>
-          <div className={style.title}>
-            <b>{}수능대박2</b>님의 책상에
-            <br />
-            <b>{}246개</b>의 응원이 왔어요!
-          </div>
-        </div>
-        <div className={style.desk}>
-          <div className={style.gridContainer}>
-            {deskLetter.slice(0, 9).map((letter, index) => (
-              <div key={index} className={style.gridItem}>
-                {/* <img src={`${letter.assetImg}`} /> */}
-                <img src={`${letter}`} />
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className={style.btn}>
-          {isMine ? (
-            <>
-              <div className={style.shareBtn} onClick={openModal}>
-                공유하기
-              </div>
-              <ShareURLModal isOpen={isModalOpen} onClose={closeModal} />
-            </>
-          ) : (
-            <>
-              <Link to="/desk/selectAsset" className={style.link}>
-                <div className={style.cheerUpBtn} onClick={() => navigate("/")}>
-                  응원하기
-                </div>
-              </Link>
-              <div className={style.myDeskBtn}>내 책상 보기</div>
-            </>
-          )}
+    // <div className={styleApp.app}>
+    <div className={style.deskPage}>
+      <img
+        src="https://wishme-bichnali.s3.ap-northeast-2.amazonaws.com/background/deskBackground.PNG"
+        className={styleApp.bg}
+      />
+      <div className={style.board}>
+        <div className={style.title}>
+          <b>{}수능대박2</b>님의 책상에
+          <br />
+          <b>{}246개</b>의 응원이 왔어요!
         </div>
       </div>
+      <div className={style.desk}>
+        <div className={style.gridContainer}>
+          {deskLetter.slice(0, 9).map((letter, index) => (
+            <div key={index} className={style.gridItem}>
+              {/* <img src={`${letter.assetImg}`} /> */}
+              <img src={`${letter}`} />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={style.btn}>
+        {isMine ? (
+          <>
+            <div className={style.shareBtn} onClick={openModal}>
+              공유하기
+            </div>
+            <ShareURLModal isOpen={isModalOpen} onClose={closeModal} />
+          </>
+        ) : (
+          <>
+            <Link to="/desk/selectAsset" className={style.link}>
+              <div className={style.cheerUpBtn} onClick={() => navigate("/")}>
+                응원하기
+              </div>
+            </Link>
+            <div className={style.myDeskBtn}>내 책상 보기</div>
+          </>
+        )}
+      </div>
     </div>
+    // </div>
   );
 };
 
