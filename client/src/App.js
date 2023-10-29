8bea979fc7fa95c30c7abc57108db3e71066a10f
zosunny <zoheyy108@naver.com>
2023-10-29 15:10:00 +0900

    [FE/Feat] : 공유하기 모달

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,22 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import MainPage from './pages/mainPage/mainPage'
-import DeskPage from './pages/deskPage/deskPage'
-import SchoolPage from './pages/schoolPage/schoolPage'
-<<<<<<< HEAD
-
-function App() {
-  return (
-=======
-import WriteDeskLetter from './pages/deskLetter/writeDeskLetter'
-import style from './app.module.css'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/mainPage/mainPage";
+import DeskPage from "./pages/deskPage/deskPage";
+import SchoolPage from "./pages/schoolPage/schoolPage";
+import WriteDeskLetter from "./pages/deskLetter/writeDeskLetter";
+import style from "./app.module.css";
 
 function App() {
   return (
     <div className={style.app}>
->>>>>>> 186da0691f8c7097e9766733f003517b51c9632a
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
         <Route path="/desk" element={<DeskPage />}></Route>
         <Route path="/desk/write" element={<WriteDeskLetter />} /> {/* 추가 */}
         <Route path="/school" element={<SchoolPage />}></Route>
       </Routes>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
