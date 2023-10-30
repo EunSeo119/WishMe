b249652dd4973a7839e7eb30a01e2830c7689d51
eunseo <qnrqjrm03@naver.com>
2023-10-30 10:46:15 +0900

    [FE/Update] 책상 페이지 수정

--- client/src/pages/deskPage/deskPage.js
+++ client/src/pages/deskPage/deskPage.js
@@ -8,21 +8,11 @@ import ShareURLModal from "../../Modal/shareURLModal";
 
 const DeskPage = () => {
   const [page, setPage] = useState(1);
-  const [userUuid, setUserUuid] = useState(1);
-  const [isMine, setIsMine] = useState(true);
+  // const [deskUuid, setDeskUuid] = useState("");
+  const [isMine, setIsMine] = useState(false);
   const [deskName, setDeskName] = useState("test");
   const [totalCount, setTotalCount] = useState(0);
-  const [deskLetter, setDeskLetter] = useState([
-    "https://wishme-bichnali.s3.ap-northeast-2.amazonaws.com/asset/desk/찹쌀떡.png",
-    "https://wishme-bichnali.s3.ap-northeast-2.amazonaws.com/asset/desk/엿.png",
-    "https://wishme-bichnali.s3.ap-northeast-2.amazonaws.com/asset/desk/딱풀.png",
-    "https://wishme-bichnali.s3.ap-northeast-2.amazonaws.com/asset/desk/초콜릿.png",
-    "https://wishme-bichnali.s3.ap-northeast-2.amazonaws.com/asset/desk/휴지.png",
-    "https://wishme-bichnali.s3.ap-northeast-2.amazonaws.com/asset/desk/포크.png",
-    "https://wishme-bichnali.s3.ap-northeast-2.amazonaws.com/asset/desk/찹쌀떡.png",
-    "https://wishme-bichnali.s3.ap-northeast-2.amazonaws.com/asset/desk/엿.png",
-    "https://wishme-bichnali.s3.ap-northeast-2.amazonaws.com/asset/desk/휴지.png",
-  ]);
+  const [deskLetter, setDeskLetter] = useState([]);
   // const [totalPage, setTotalPage] = useState(1)
   const navigate = useNavigate();
 
@@ -38,8 +28,16 @@ const DeskPage = () => {
   // var url = 'http://localhost:8082/'
 
   useEffect(() => {
-    axios
-      .get(`/api/my/letter/all/${userUuid}?page=${page}`)
+    const AccessToken = localStorage.getItem("AccessToken");
+    const DeskUuid = localStorage.getItem("deskUuid");
+    axios({
+      method: "get",
+      url: `http://localhost:8080/api/my/letter/all/${DeskUuid}?page=${page}`,
+      headers: {
+        Authorization: `Bearer ${AccessToken}`,
+      },
+    })
+      // .get(`http://localhost:8080/api/my/letter/all/${userUuid}?page=${page}`)
       .then((response) => {
         const data = response.data;
         console.log(data);
@@ -47,22 +45,6 @@ const DeskPage = () => {
         setTotalCount(data.totalLetterCount);
         setDeskLetter(data.myLetterResponseDtoList);
         setIsMine(data.isMine);
-        // setTotalPage(data.totalPage)
-      })
-      .catch((error) => {
-        console.error("API 요청 중 오류 발생:", error);
-      });
-  }, [userUuid, page]);
-
-  useEffect(() => {
-    axios
-      .get(`/api/my/letter/all/${userUuid}?page=${page}`)
-      .then((response) => {
-        const data = response.data;
-        console.log(data);
-        setDeskName(data.toUserNickname);
-        setTotalCount(data.totalLetterCount);
-        setDeskLetter(data.myLetterResponseDtoList);
         // setTotalPage(data.totalPage)
       })
       .catch((error) => {
@@ -79,9 +61,9 @@ const DeskPage = () => {
         />
         <div className={style.board}>
           <div className={style.title}>
-            <b>{}수능대박2</b>님의 책상에
+            <b>{deskName}</b>님의 책상에
             <br />
-            <b>{}246개</b>의 응원이 왔어요!
+            <b>{totalCount}</b>개의 응원이 왔어요!
           </div>
         </div>
         <div className={style.desk}>
@@ -89,7 +71,7 @@ const DeskPage = () => {
             {deskLetter.slice(0, 9).map((letter, index) => (
               <div key={index} className={style.gridItem}>
                 {/* <img src={`${letter.assetImg}`} /> */}
-                <img src={`${letter}`} />
+                <img src={`${letter.assetImg}`} />
               </div>
             ))}
           </div>
