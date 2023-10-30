3fb2a4e54a184b876990229de1ad0f8e88a7654f
조해린 <stella9808@naver.com>
2023-10-30 13:20:16 +0900

    [FE/feat] : 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import MainPage from './pages/mainPage/mainPage'
+import MyPage from './pages/mypage/myPage'
 import KakaoRedirectPage from './pages/mainPage/kakaoRedirectPage'
 import DeskPage from './pages/deskPage/deskPage'
 import SchoolPage from './pages/schoolPage/schoolPage'
@@ -16,8 +17,9 @@ function App() {
     <div className={style.app}>
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
+        <Route path="/mypage" element={<MyPage />}></Route>
         <Route path="/kakao/callback" element={<KakaoRedirectPage />}></Route>
-        <Route path="/desk" element={<DeskPage />}></Route>
+        <Route path="/desk/:deskUuid" element={<DeskPage />}></Route>
         <Route path="/desk/selectAsset" element={<SelectDeskAsset />} />
         <Route
           path="/desk/writeLetter/:assetSeq"
