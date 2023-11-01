22d7826843d2b970ae01a13c962e94340f4f5f26
wnstj7788 <wnstj7788@gmail.com>
2023-11-01 10:32:29 +0900

    feature sidebar Edit

--- client/src/Common/Header.js
+++ client/src/Common/Header.js
@@ -1,15 +1,45 @@
-import React, {useState} from 'react';
-import {useNavigate} from 'react-router';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router';
 import style from './Header.module.css';
-import toast, {Toaster} from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
+import axios from "axios";
 
 const Header = () => {
     const navigate = useNavigate();
+    const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+
 
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [UserName, setUserName] = useState(""); // 상태로 UserName 관리
+
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen);
     }
+
+    useEffect(() => {
+        const AccessToken = localStorage.getItem("AccessToken");
+        if (AccessToken) {
+            const headers = {
+                Authorization: `Bearer ${AccessToken}`
+            };
+
+            axios({
+                method: "get",
+                url: `${SERVER_URL}/api/users`,
+                headers,
+            })
+                .then((response) => {
+                    const data = response.data;
+                    if (data) {
+                        setUserName(data.userNickname); // 데이터가 있으면 상태 업데이트
+                    }
+                })
+                .catch((error) => {
+                    console.log(SERVER_URL);
+                    console.error("API 요청 중 오류 발생:", error);
+                });
+        }
+    }, []); // 빈 배열을 넘겨 한 번만 호출되도록 설정
 
     // 링크를 클릭하면 해당 페이지로 이동하는 함수
     const handleLinkClick = (path) => {
@@ -19,51 +49,66 @@ const Header = () => {
 
     return (
         <div className={style.header}>
-            <Toaster/>
+            <Toaster />
             <div onClick={toggleSidebar}>
-                <img src="/assets/Menu.png" className={style.menuIcon}/>
+                <img src="/assets/Menu.png" className={style.menuIcon} />
             </div>
             <div className={`${style.sidebar} ${isSidebarOpen ? style.open : ''}`}>
-                <div className={style.sideName}>TEST님</div>
+                <div className={style.sideName}> {UserName} </div>
                 <div>반갑습니다!</div>
 
                 <br></br>
-                <div
-                    onClick={() => handleLinkClick('/')}
-                    style={{
-                        cursor: 'pointer',
-                        padding: '10px',
-                        borderBottom: '1px solid #ccc',
-                    }}
-                >
-                    홈
-                </div>
-                <div
-                    onClick={() => handleLinkClick('/mypage')}
-                    style={{
-                        cursor: 'pointer',
-                        padding: '10px',
-                        borderBottom: '1px solid #ccc',
-                    }}
-                >
-                    마이페이지 </div>
-                <div
-                    onClick={() => handleLinkClick('/')}
-                    style={{
-                        cursor: 'pointer',
-                        padding: '10px',
-                        borderBottom: '1px solid #ccc',
-                    }}
-                >개발자 책상 가기</div>
-                <div
-                    onClick={() => handleLinkClick('/')}
-                    style={{
-                        cursor: 'pointer',
-                        padding: '10px',
-                        borderBottom: '1px solid #ccc',
-                    }}
-                >로그아웃</div>
-
+                {UserName ? (
+                    <>
+                        <div
+                            onClick={() => handleLinkClick('/mypage')}
+                            style={{
+                                cursor: 'pointer',
+                                padding: '10px',
+                                borderBottom: '1px solid #ccc',
+                            }}
+                        >
+                            마이페이지
+                        </div>
+                        <div
+                            onClick={() => handleLinkClick('/')}
+                            style={{
+                                cursor: 'pointer',
+                                padding: '10px',
+                                borderBottom: '1px solid #ccc',
+                            }}
+                        >로그아웃</div>
+                    </>
+                ) : (
+                    <>
+                        <div
+                            onClick={() => handleLinkClick('/login')}
+                            style={{
+                                cursor: 'pointer',
+                                padding: '10px',
+                                borderBottom: '1px solid #ccc',
+                            }}
+                        >
+                            로그인하기
+                        </div>
+                        <div
+                            onClick={() => handleLinkClick('/developer-desk')}
+                            style={{
+                                cursor: 'pointer',
+                                padding: '10px',
+                                borderBottom: '1px solid #ccc',
+                            }}
+                        >개발자 책상 가기</div>
+                        <div
+                            onClick={() => handleLinkClick('/school-support')}
+                            style={{
+                                cursor: 'pointer',
+                                padding: '10px',
+                                borderBottom: '1px solid #ccc',
+                            }}
+                        >학교 응원하기</div>
+                    </>
+                )}
             </div>
             {isSidebarOpen && (
                 <div className={style.overlay} onClick={toggleSidebar}></div>
