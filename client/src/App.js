3fa3a99a737e5b5d2ed7e0d3692d86e65d6f787b
허다은 <qor_4@naver.com>
2023-11-02 13:28:31 +0900

    [FE/Feat] 링크 접속 후 로그인 시 버그

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import MainPage from './pages/mainPage/mainPage'
+import MainPageUuid from './pages/mainPage/mainPageUuid'
 import MyPage from './pages/mypage/myPage'
 import KakaoRedirectPage from './pages/mainPage/kakaoRedirectPage'
 import DeskPage from './pages/deskPage/deskPage'
@@ -22,6 +23,7 @@ function App() {
     <div className={style.app}>
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
+        <Route path="/login/:deskUuid" element={<MainPageUuid />}></Route>
         <Route path="/searchSchool" element={<SearchSchoolPage />}></Route>
         <Route path="/mypage" element={<MyPage />}></Route>
         <Route path="/kakao/callback" element={<KakaoRedirectPage />}></Route>
