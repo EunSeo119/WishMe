711b5de1d45480b5f6c0a581fe090794f6811a6e
조해린 <stella9808@naver.com>
2023-11-02 13:29:07 +0900

    [FE/Feat]: 사이드바 수정

--- client/src/pages/searchSchoolPage/searchSchoolPage.js
+++ client/src/pages/searchSchoolPage/searchSchoolPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import style from './searchSchoolPage.module.css'
 import axios from 'axios'
 import { useNavigate } from "react-router-dom";
+import { IoIosArrowBack } from 'react-icons/io'
 
 const SearchSchoolPage = () => {
 
@@ -21,6 +22,11 @@ const SearchSchoolPage = () => {
         }else{
             alert("학교가 선택되지 않았습니다!")
         }
+    }
+
+    //이전으로
+    const onClickBeforeBtn = () => {
+        navigate(-1)
     }
 
     // 엔터키로 동작
@@ -68,39 +74,45 @@ const SearchSchoolPage = () => {
     }
 
     return (
-        <div className={style.container}>
-            <div className={style.school}>
-                <div style={{width: '100%', fontSize: '35px', marginBottom: '30px'}}>학교 검색하기</div>
-                <div className={style.desc1}> * 학교를 선택하면 해당 학교에 온 응원을 볼 수 있어요!</div>
-                <div className={style.desc2} onClick={goMypage}> 마이페이지에서 내 학교 등록하기</div>
-                <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
-                    <div>학교 :</div>
-                    <div>
-                    <input type="text" value={tempSchoolName} onChange={(e) => changeSchool(e)} onKeyPress={handleKeyPress}/>
-                    </div>
-                    <div className={style.searchBtn} onClick={searchSchool}>
-                    검색
-                    </div>
-                </div>
-                {schoolList.length > 0 ? (
-                    <>
-                    <div className={style.schoolList}>
-                        <ul>
-                        {schoolList.map((school, idx) => (
-                            <li key={school.schoolSeq} onClick={() => selectSchool(school.schoolName, school.uuid, idx)}
-                            style={{ backgroundColor: selectedIdx === idx ? '#ececec' : 'white'}}> {school.schoolName}<br />
-                            <div style={{ color: '#aeaeae' }}>{school.region}</div>
-                            <hr />
-                            </li>
-                        ))}
-                        </ul>
-                    </div></>
-                ) : (
-                    <>
-                    </>
-                )}
+        <div>
+            <div className={style.beforeTitle} onClick={() => onClickBeforeBtn()}>
+                <IoIosArrowBack className={style.icon} />
+                이전으로
             </div>
-            <div className={style.completeBtn} onClick={saveClick}>다음</div>
+            <div className={style.container}>
+                <div className={style.school}>
+                    <div style={{width: '100%', fontSize: '35px', marginBottom: '30px'}}>학교 검색하기</div>
+                    <div className={style.desc1}> * 학교를 선택하면 해당 학교에 온 응원을 볼 수 있어요!</div>
+                    <div className={style.desc2} onClick={goMypage}> 마이페이지에서 내 학교 등록하기</div>
+                    <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+                        <div>학교 :</div>
+                        <div>
+                        <input type="text" value={tempSchoolName} onChange={(e) => changeSchool(e)} onKeyPress={handleKeyPress}/>
+                        </div>
+                        <div className={style.searchBtn} onClick={searchSchool}>
+                        검색
+                        </div>
+                    </div>
+                    {schoolList.length > 0 ? (
+                        <>
+                        <div className={style.schoolList}>
+                            <ul>
+                            {schoolList.map((school, idx) => (
+                                <li key={school.schoolSeq} onClick={() => selectSchool(school.schoolName, school.uuid, idx)}
+                                style={{ backgroundColor: selectedIdx === idx ? '#ececec' : 'white'}}> {school.schoolName}<br />
+                                <div style={{ color: '#aeaeae' }}>{school.region}</div>
+                                <hr />
+                                </li>
+                            ))}
+                            </ul>
+                        </div></>
+                    ) : (
+                        <>
+                        </>
+                    )}
+                </div>
+                <div className={style.completeBtn} onClick={saveClick}>다음</div>
+            </div>
         </div>
     );
 };
