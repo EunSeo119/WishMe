c073b48343d5320121f492455eed8d30746bcff3
chlgksdbs <chlgksdbs98@naver.com>
2023-11-06 10:38:26 +0900

    Feat: Refresh Token을 통한 Access Token 재발급 로직 구현

--- server/user/src/main/java/com/wishme/user/user/model/service/UserService.java
+++ server/user/src/main/java/com/wishme/user/user/model/service/UserService.java
@@ -12,4 +12,5 @@ public interface UserService {
     ResponseEntity<?> registerSchool(Map<String, String> request, Long userSeq);
     ResponseEntity<?> searchSchool(SearchSchoolRequestDto searchSchoolRequestDto);
     ResponseEntity<?> getUserInfo(Long userSeq);
+    ResponseEntity<?> getAccessTokenByRefreshToken(String refreshToken);
 }
