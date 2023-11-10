f03106410c51c3b17cd829d113dcfe3b004f8c82
허다은 <qor_4@naver.com>
2023-11-10 17:34:37 +0900

    [FE/Fix] script 수정

--- client/src/Clarity.js
+++ client/src/Clarity.js
@@ -5,7 +5,9 @@ const Clarity = () => {
     return (
         <div>
             <Helmet>
-                <script type="text/javascript" >
+  <script type="text/javascript" src="https://www.clarity.ms/tag/jo82kvcijt"></script>
+
+                {/* <script type="text/javascript" >
                     {`
                     (function(c,l,a,r,i,t,y){
                         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
@@ -13,7 +15,7 @@ const Clarity = () => {
                         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
                     })(window, document, "clarity", "script", "jo82kvcijt");
             `}
-                </script>
+                </script> */}
             </Helmet>
             {/* 나머지 컴포넌트 내용 */}
         </div>
